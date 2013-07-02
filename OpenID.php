ac74e35a18779c9d938da43a0e125e5bb04d77b4
kafene <matt@kafene.org>
2013-07-02 02:18:18 -0400

    yay i made it work again

--- OpenID.php
+++ OpenID.php
@@ -8,56 +8,59 @@
  * @todo this stopped working but I still get sreg info?
  * # echo openid_verify();
 */
+echo openid_verify();
 function openid_verify($return_to = null, array $params = []) {
-    if(null == $return_to) {
+    if(!$return_to) {
         $return_to = 'http://'.getenv('HTTP_HOST').(getenv('REQUEST_URI') ?: '/');
     }
     if(isset($_POST['openid_verify'])) {
-        $ch = curl_init($_POST['openid_verify']);
-        curl_setopt_array($ch, [
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_SSL_VERIFYHOST => false,
-            CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_HTTPHEADER => ['Accept: application/xrds+xml'],
-        ]);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        $next = simplexml_load_string($response)->XRD->Service->URI;
-        $id_select = 'http://specs.openid.net/auth/2.0/identifier_select';
+        $url = $_POST['openid_verify'];
         $need = [
             'openid.mode' => 'checkid_setup',
             'openid.ns' => 'http://specs.openid.net/auth/2.0',
-            'openid.claimed_id' => $id_select,
-            'openid.identity' => $id_select,
+            'openid.claimed_id' => 'http://specs.openid.net/auth/2.0/identifier_select',
+            'openid.identity' => 'http://specs.openid.net/auth/2.0/identifier_select',
             'openid.return_to' => $return_to,
-            'openid.realm' => $return_to
+            'openid.realm' => $return_to,
         ];
-        $params = array_merge($need, $params);
-        $q = parse_url($next, PHP_URL_QUERY) ? '&' : '?';
-        $next .= $q.http_build_query($params);
+        $meta = get_meta_tags($url);
+        if(isset($meta['x-xrds-location'])) {
+            $url = $meta['x-xrds-location'];
+        }
+        $c = stream_context_create(['http' => [
+            'method' => 'GET',
+            'headers' => 'Accept: application/xrds+xml',
+        ]]);
+        $response = file_get_contents($url, null, $c);
+        libxml_use_internal_errors(true);
+        $xrds = simplexml_load_string($response);
+        if(isset($xrds->XRD->Service->URI)) {
+            $next = $xrds->XRD->Service->URI;
+        }
+        $params = array_merge($params, $need);
+        $qs = parse_url($next, PHP_URL_QUERY);
+        $next .= isset($qs) ? '&' : '?';
+        $next .= http_build_query($params);
         header('Location: '.$next);
         exit(0);
-    } elseif(
-        isset($_GET['openid_mode'], $_GET['openid_op_endpoint'])
-        && $_GET['openid_mode'] == 'id_res'
-    ) {
-        $ch = curl_init($_GET['openid_op_endpoint']);
-        $next = str_replace(
+    } elseif(isset($_GET['openid_mode'], $_GET['openid_op_endpoint']) && $_GET['openid_mode'] == 'id_res') {
+        $url = $_GET['openid_op_endpoint'];
+        $data = str_replace(
             'openid.mode=id_res',
             'openid.mode=check_authentication',
             getenv('QUERY_STRING')
         );
+        $ch = curl_init($url);
         curl_setopt_array($ch, [
             CURLOPT_POST => true,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_SSL_VERIFYHOST => false,
             CURLOPT_SSL_VERIFYPEER => false,
-            CURLOPT_POSTFIELDS => [$next]
+            CURLOPT_POSTFIELDS => $data
         ]);
         $response = curl_exec($ch);
         curl_close($ch);
-        $valid = false !== stripos($response, 'is_valid:true');
-        return compact('response', 'valid');
+        return 0 === stripos(trim($response), 'is_valid:true');
     } else {
         return '
             <form method="post" action="" class="openid">
